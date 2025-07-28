1ba6accf332d724667a9cce26fff5d708a6ea022
psmithqts <preston.smith@qtsdatacenters.com>
2025-07-28 13:57:35 -0500

    Update single_display.ps1

--- single_display.ps1
+++ single_display.ps1
@@ -1,4 +1,5 @@
 $destination = "C:\Users\Skype\AppData\Local\Packages\Microsoft.SkypeRoomSystem_8wekyb3d8bbwe\LocalState"
+cd $destination
 
 # Download files from GitHub
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/psmithqts/single_display/main/MTR_BG_Extend.png" -OutFile "$destination\MTR_BG_Extend.png"
